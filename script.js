0b1f140d7ea84ae58ea0eba2893d4726a891631a
Kevin Japa <84851902+kevinjapa@users.noreply.github.com>
2024-12-18 00:29:45 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Mostrar botón flotante al hacer scroll
+const backToTop = document.getElementById('back-to-top');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        backToTop.classList.remove('hidden');
+    } else {
+        backToTop.classList.add('hidden');
+    }
+});
