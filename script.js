2d52f60716099b8bc3137c93b3df11aef8e8401e
juanjapa001@gmail.com <Japakevin001@gmail.com>
2026-08-17 22:32:26 -0500

    Update cv and add file university certificate

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // Mostrar botón flotante al hacer scroll
 const backToTop = document.getElementById('back-to-top');
-
 window.addEventListener('scroll', () => {
     if (window.scrollY > 300) {
         backToTop.classList.remove('hidden');
     } else {
         backToTop.classList.add('hidden');
     }
+});
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("year").textContent = new Date().getFullYear();
 });
